9034caa42d55e79bd66d5e4ee5b4cb6c27670f36
yusufr98 <yusuf090798@gmail.com>
2019-10-21 16:29:35 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double mynumber=2;
+    double myothernumber=1.7938;
+    System.out.println(mynumber);
+    System.out.println(myothernumber);
+    }
+}
